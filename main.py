72379bde6c4bfe1a67ebfec18da84c9f1a0059bf
Bastidoff <daniel.oss@gmail.com>
2022-08-11 08:35:17 -0500

    ntj se crea main con datos primitivos y entradas por teclado

---
+++ main.py
@@ -0,0 +1,12 @@
+#1. datos primitivos
+nombre="Daniel"
+edad=35
+estatura=1.82
+hinchaDelMejor=True
+comidaFavorita=None #variable sin inicializar, pero en Python como no hay palabra reservada para declarar variables, hay que iniciliazarla con None
+#2. Datos que entran por el teclado
+numero1=int(input("Digite un número: "))
+numero2=int(input("Digite el segundo múmero: "))
+suma=numero1+numero2
+
+print(f'la suma de {numero1} y {numero2} es {suma}')
